97b4c75d267c21608b199f3c74a5cc5907049d76
Rupen Bhimani <rupenbhimani109@gmail.com>
2022-12-18 19:55:47 +0530

    fix chat context

---
+++ src/context/ChatContext.js
@@ -0,0 +1,26 @@
+import { createContext, useEffect, useState } from "react";
+import { auth } from "../Firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+export const ChatContext = createContext();
+
+export const ChatContextProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState({});
+
+    useEffect(() => {
+        const unsub = onAuthStateChanged(auth, (user) => {
+            setCurrentUser(user);
+            console.log(user);
+        });
+
+        return () => {
+            unsub();
+        };
+    }, []);
+
+    return (
+        <ChatContext.Provider value={{ currentUser }}>
+            {children}
+        </ChatContext.Provider>
+    );
+};
