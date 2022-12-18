cd99f64b9b26f24c6dd86327f7a2a33d8ea1bef2
Rupen Bhimani <rupenbhimani109@gmail.com>
2022-12-18 15:59:22 +0530

    set condtions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
+// import Home from "./pages/Home";
+// import Register from "./pages/Register";
+// import Login from "./pages/Login";
+// import "./style.scss";
+// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import { useContext } from "react";
+// import { AuthContext } from "./context/AuthContext";
+
+// function App() {
+//   const { currentUSer } = useContext(AuthContext);
+//   const ProtectedRoute = ({ children }) => {
+//     if (!currentUSer) {
+//       return <Navigate to="/login" />;
+//     }
+//   }
+//   return (
+     
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/">
+//           <Route index element={
+//           <ProtectedRoute>
+//             <Home />
+//           </ProtectedRoute>
+          
+//           } />
+//           <Route path="login" element={<Login />} />
+//           <Route path="register" element={<Register />} />
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+
+//   );
+// }
+
+// export default App;
+
+
 import Home from "./pages/Home";
-import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import "./style.scss";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
-  return (
-    // <Home />
-    // <Register />
+  const { currentUser } = useContext(AuthContext);
 
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children
+  };
+
+  return (
     <BrowserRouter>
       <Routes>
         <Route path="/">
-          <Route index element={<Home />} />
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
         </Route>
       </Routes>
     </BrowserRouter>
-
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../Firebase";
 
 const Navbar = () => {
-  return (
-      <div className="navbar">
-          <span className="logo">Lama Chat</span>
-          <div className="user">
-              <img src="https://images.pexels.com/photos/879143/pexels-photo-879143.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-              <span>john edfedf</span>
-              <button>logout</button>
-          </div>
-      </div>
-  );
-}
+    return (
+        <div className="navbar">
+            <span className="logo">Lama Chat</span>
+            <div className="user">
+                <img src="https://images.pexels.com/photos/879143/pexels-photo-879143.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                <span>john edfedf</span>
+                <button onClick={()=> signOut(auth)}>logout</button>
+            </div>
+        </div>
+    );
+};
 
 export default Navbar;
