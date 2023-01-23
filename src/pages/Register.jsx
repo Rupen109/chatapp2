29ef5acffcbdb56c1db30c6167c6790304ea178b
Rupen Bhimani <rupenbhimani109@gmail.com>
2023-01-23 23:06:03 +0530

    sign up and login page update

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -145,7 +145,7 @@ const Register = () => {
     return (
         <div className="formContainer">
             <div className="formWrapper">
-                <span className="logo">Lama Chat</span>
+                <span className="logo">panchat</span>
                 <span className="title">Register</span>
                 <form onSubmit={handleSubmit}>
                     <input required type="text" placeholder="display name" />
@@ -161,7 +161,7 @@ const Register = () => {
                     {err && <span>Something went wrong</span>}
                 </form>
                 <p>
-                    You do have an account? <Link to="/register">Login</Link>
+                    You do have an account? <Link to="/login">Login</Link>
                 </p>
             </div>
         </div>
