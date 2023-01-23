62163c44382fea817b02505db2e309f67c34e10b
Rupen Bhimani <rupenbhimani109@gmail.com>
2023-01-23 17:24:41 +0530

    register user page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -76,7 +76,6 @@
 
 // export default Register;
 
-
 import React, { useState } from "react";
 import Add from "../img/7224509.png";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
@@ -84,6 +83,7 @@ import { auth, db, storage } from "../Firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
 import { useNavigate, Link } from "react-router-dom";
+// import {  ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 const Register = () => {
     const [err, setErr] = useState(false);
@@ -102,36 +102,41 @@ const Register = () => {
             //Create user
             const res = await createUserWithEmailAndPassword(auth, email, password);
 
-            //Create a unique image name
-            const date = new Date().getTime();
-            const storageRef = ref(storage, `${displayName + date}`);
+            const storageRef = ref(storage, displayName);
 
-            await uploadBytesResumable(storageRef, file).then(() => {
-                getDownloadURL(storageRef).then(async (downloadURL) => {
-                    try {
-                        //Update profile
-                        await updateProfile(res.user, {
-                            displayName,
-                            photoURL: downloadURL,
-                        });
-                        //create user on firestore
-                        await setDoc(doc(db, "users", res.user.uid), {
-                            uid: res.user.uid,
-                            displayName,
-                            email,
-                            photoURL: downloadURL,
-                        });
-
-                        //create empty user chats on firestore
-                        await setDoc(doc(db, "userChats", res.user.uid), {});
-                        navigate("/");
-                    } catch (err) {
-                        console.log(err);
-                        setErr(true);
-                        setLoading(false);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+                         
+            // Register three observers:
+            // 1. 'state_changed' observer, called any time the state changes
+            // 2. Error observer, called on failure
+            // 3. Completion observer, called on successful completion
+            uploadTask.on(
+                "state_changed",
+                (snapshot) => {
+                    // Observe state change events such as progress, pause, and resume
+                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log("Upload is " + progress + "% done");
+                    switch (snapshot.state) {
+                        case "paused":
+                            console.log("Upload is paused");
+                            break;
+                        case "running":
+                            console.log("Upload is running");
+                            break;
                     }
-                });
-            });
+                },
+                (error) => {
+                    // Handle unsuccessful uploads
+                },
+                () => {
+                    // Handle successful uploads on complete
+                    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        console.log("File available at", downloadURL);
+                    });
+                }
+            );
         } catch (err) {
             setErr(true);
             setLoading(false);
@@ -141,7 +146,7 @@ const Register = () => {
     return (
         <div className="formContainer">
             <div className="formWrapper">
-                <span className="logo">Lama Chat</span>
+                <span className="logo">Panchat</span>
                 <span className="title">Register</span>
                 <form onSubmit={handleSubmit}>
                     <input required type="text" placeholder="display name" />
@@ -157,7 +162,7 @@ const Register = () => {
                     {err && <span>Something went wrong</span>}
                 </form>
                 <p>
-                    You do have an account? <Link to="/register">Login</Link>
+                    You do have an account? <Link to="/login">Login</Link>
                 </p>
             </div>
         </div>
